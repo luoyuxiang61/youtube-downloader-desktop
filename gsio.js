89a0a75bb444efbc822f4e6b32a0f9420ba71595
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-02-08 14:49:13 +0800

    add progress

--- gsio.js
+++ gsio.js
@@ -1,17 +1,21 @@
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
+const crypto = require('crypto')
 
 
 let linkInput = document.getElementById('linkInput')
 let downloadBtn = document.getElementById('downloadBtn')
 let giveMeLink = document.getElementById('giveMeLink')
 
+let progressCon = document.getElementById('progressCon')
+let progress = document.getElementById('progress')
+let progressTip = document.getElementById('progressTip')
+
 
 giveMeLink.addEventListener('click', (e) => {
     let videoLink = linkInput.value
     let encodedVideoLink = [...videoLink].map(x => x.codePointAt(0)).toString()
-
     http.get({
         hostname: 'geekshine.io',
         port: 3000,
@@ -26,6 +30,7 @@ giveMeLink.addEventListener('click', (e) => {
             console.log(info)
             downloadBtn.style.display = 'block'
             downloadBtn.hashName = info.videoName
+            downloadBtn.videoSize = info.videoSize
         })
     })
 })
@@ -38,8 +43,10 @@ linkInput.addEventListener('keydown', () => {
 
 downloadBtn.addEventListener('click', (evt) => {
     downloadBtn.style.display = 'none'
-    let videoTimeName = evt.target.hashName
-    let videoPath = path.join(__dirname, `${videoTimeName}`)
+    progressCon.style.display = 'block'
+    let videoTimeName = crypto.createHmac('sha256', new Date().toTimeString() + new Date().toDateString()).update('i love nodejs').digest('hex').substr(2, 10)
+    let videoSize = parseFloat(evt.target.videoSize) * 1048576
+    let videoPath = path.join(__dirname, `${videoTimeName}.mp4`)
     fs.open(videoPath, 'w', (err, fd) => {
         let hashName = evt.target.hashName
         http.get({
@@ -47,11 +54,19 @@ downloadBtn.addEventListener('click', (evt) => {
             port: 3000,
             path: `/downloadByHashName?hashName=${hashName}`
         }, (res) => {
+
+            let size = 0;
+            res.on('data', (chunk) => {
+                size += chunk.length
+                var p = Math.round((size / videoSize) * 100)
+                progress['aria - valuenow'] = p
+                progress.style.width = `${p}%`
+                progressTip.innerText = `${p}%`
+            })
+
             res.on('end', () => {
-                alert('download ok boy')
-                fs.stat(videoPath, (err, stats) => {
-                    console.log(stats)
-                })
+                progressCon.style.display = 'none'
+                alert('ok!')
             })
             res.pipe(fs.createWriteStream(videoPath))
         })
